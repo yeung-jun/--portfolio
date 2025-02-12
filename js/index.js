fc6b2ad4c38c64add92729e3d28ccf075ae3b9eb
yeung-jun <yeungjun267@gmail.com>
2025-02-12 13:14:42 +0900

    update

--- js/index.js
+++ js/index.js
@@ -14,4 +14,16 @@ $(function(){
     $('#project article').click(function(){
         $(this).toggleClass('view').siblings().removeClass('view')
     });
+    const screenWidth = $(window).width();
+    if(screenWidth >= 1201) {
+      $('#project article.sum41').click(function(){
+        window.location.href= "../sum41/index.html";
+      });
+      $('#project article.han').click(function(){
+        window.location.href= "../hanchangboo/index.html";
+      });
+      $('#project article.synth').click(function(){
+        window.location.href= "../synth_wave_ani/index.html";
+      });
+    }
 });
